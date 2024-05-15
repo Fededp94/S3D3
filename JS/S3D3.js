3862bf76d7d90f2402e091a628d5b2a125020f7d
Federico Di Presa <federicodipresa94@gmail.com>
2024-05-15 22:03:45 +0200

    Esercizio 1505
    Correzione e completamento esercizio

--- JS/S3D3.js
+++ JS/S3D3.js
@@ -1,9 +1,20 @@
-const newTask = document.getElementById("form");
-newTask.addEventListener("submit", function (event) {
-  event.preventDefault();
-  const taskList = document.getElementById("taskList");
-  const nomeTask = taskList.value;
-  const creazioneLista = document.createElement("li");
-  creazioneLista.innerText = "$[nomeTask]";
-  creazioneLista.appendChild("ul");
+const form = document.getElementById("form");
+form.addEventListener("submit", function (evento) {
+  evento.preventDefault();
+  // console.log("inviato");
+  const input = document.getElementById("taskTesto").value; // ottengo il valore dell'input
+  const lista = document.createElement("li");
+  lista.innerText = input; // inserisco il valore dell'input nel nuovo elemento della lista
+  lista.classList.add("listaCss");
+  document.getElementById("taskTesto").value = ""; //resetto il valore dell'input
+  document.getElementById("taskList").appendChild(lista); //aggiungo l'elemento alla lista
+
+  const buttonElimina = document.createElement("button");
+  buttonElimina.innerText = "Canc";
+
+  buttonElimina.addEventListener("click", function () {
+    lista.remove(); //rimuovo l'elemento della lista quando il pulsante viene premuto
+  });
+
+  lista.appendChild(buttonElimina); //aggiungo il pulsante all'elemento della lista
 });
